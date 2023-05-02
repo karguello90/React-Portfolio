d38481798464ce274318de1c094d0a315fd5fc4e
karguello90 <karguello90@gmail.com>
2023-05-01 20:34:33 -0400

    updating with third group project

--- src/components/Project.js
+++ src/components/Project.js
@@ -6,6 +6,7 @@ function Project() {
     <h2>Team Projects &#9981;</h2>
     <a href={"https://sumnerfrank.github.io/dude-wash-my-car/"}><img src="./Dude wash my car.png"></img></a>
     <a href={"https://github.com/SumnerFrank/fitfolio"}><img src="./fitfolio.png"></img></a>
+    <a href={"https://fitfolio-v2.herokuapp.com"}><img src="./fitfoliov2.jpg"></img></a>
    </div>
   );
 }
